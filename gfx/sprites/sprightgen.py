a3dbac3a2ab426eecc0967e5e58389e41a227985
carrexxii <cjbarrett101@outlook.com>
2024-03-17 12:04:01 +0200

    sprightgen.py

---
+++ gfx/sprites/sprightgen.py
@@ -0,0 +1,37 @@
+import os
+
+output = os.path.join(os.path.dirname(__file__), "spright.conf")
+print(output)
+
+directions = ["sw", "s", "se", "e", "ne", "n", "nw", "w"]
+
+sheet_tmpl = \
+r"""
+description "{0}.lua"
+	template "lua.inja"
+
+sheet "{0}"
+	output "sheets/{0}.png"
+		# maps
+	pack compact
+	power-of-two true
+	square       true
+	allow-rotate true
+"""
+
+sprite_tmpl = \
+r"""
+glob "renders/{0}-{1}-{2}-*.png"
+	id "{0}@{1}@{2}" """
+
+sprites = {
+	"player": [ "run", "torch" ],
+}
+
+with open(output, "w+") as file:
+	for sprite in sprites:
+		file.write(sheet_tmpl.format(sprite))
+		for state in sprites[sprite]:
+			for dir in directions:
+				file.write(sprite_tmpl.format(sprite, state, dir))
+			file.write("\n")
